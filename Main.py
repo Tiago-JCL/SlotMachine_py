c07319468dff083f238f4707d447862d7e2eb8c0
volturifox <tiagoleite.work@gmail.com>
2023-12-23 00:19:26 -0300

    Printable Slots
    Forgot to add the columns to the list, did it in line 32. Now it prints the slot machine! Winning, losing and replaying coming tomorow!

--- Main.py
+++ Main.py
@@ -22,18 +22,19 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(symbol_count):
             all_symbols.append(symbol)
     columns = []
-    for col in range(cols):
+    for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
-        for row in range(rows):
+        for _ in range(rows):
             value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
+        columns.append(column)
     return columns
 
 
 def print_slot_machine(columns):
-    for row in range(len(columns)):
+    for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
                 print(column[row], "|", end=" | ")
